ca79b2d4714536918cf9b3ff1d7166e693e8d7f1
awaisjbr <awaisjbr@gmail.com>
2025-08-26 15:29:15 +0500

    26-08-2025

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,52 @@ import dotenv from "dotenv";
 import http from "http";
 import {Server} from "socket.io";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
+import { router as authRouter } from "./routes/auth.route.js";
+import {router as messageRouter} from "./routes/message.route.js"
 
 
 dotenv.config();
 const port = process.env.PORT;
+const allowedOrigins = ['https://mern-chat-app-orpin-psi.vercel.app', 'http://localhost:3000'];
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
+export const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000'],
+        origin: allowedOrigins,
         methods: ["GET","POST"]
     }
-})
+});
+
+const onlineUsers = {};
+export function getReceiverSocketId(userId){
+    return onlineUsers[userId]
+};
+io.on("connection", (socket) => {
+    console.log("A User coneected", socket.id);
+   const userId = socket.handshake.query.userId;
+    if(userId){
+        onlineUsers[userId]= socket.id;
+    }
+    io.emit("getOnlineUsers", Object.keys(onlineUsers));
+
+    socket.on("disconnect", () => {
+        console.log("A User Disconnected", socket.id)
+        delete onlineUsers[userId];
+        io.emit("getOnlineUsers", Object.keys(onlineUsers));
+    })
+});
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true
-}))
+}));
+app.use("/api/auth", authRouter);
+app.use("/api/message", messageRouter);
 app.get("/", (req, res) => {
     res.send("Backend working fine..")
 });
